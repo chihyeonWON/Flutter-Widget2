84c24b2f8bcc2cb9d168b3ba8475f3a27574c262
ylose <heungeob1003@gmail.com>
2022-12-31 14:37:18 +0900

    CheckBox, Switch 실습

--- lib/main.dart
+++ lib/main.dart
@@ -17,19 +17,48 @@ class MyApp extends StatelessWidget {
 // 여기까지는 공통코드
 
 //여기부터 수정
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var _isChecked = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('TextField'),
-        ),
-        body:TextField(
-          decoration:InputDecoration(
-            border:OutlineInputBorder(),
-            labelText:'여기에 입력하세요',
-          ),
+      appBar:AppBar(
+        title:Text('CheckBox / Radio / Switch'),
+      ),
+      body: Padding(
+        padding:const EdgeInsets.all(8.0),
+        child:Center(
+          child:Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children:<Widget>[
+              Checkbox(
+                value:_isChecked,
+                onChanged:(value) {
+                  setState(() {
+                    _isChecked = true;
+                  });
+                }
+              ),
+              SizedBox(
+                height:40,
+              ),
+              Switch(
+                value:_isChecked,
+                onChanged:(value){
+                  setState((){
+                    _isChecked=true;
+                  });
+                }
+              )
+            ]
+          )
         )
+      )
     );
   }
 }
